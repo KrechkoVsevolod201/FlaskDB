84abe143057f8aac5a2844e2f8ffb9ca9b2aeccf
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2021-12-18 22:56:52 +0400

    The very beginning

--- app.py
+++ app.py
@@ -3,3 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return "Gachimuchi"
+
+
+@app.route('/about')
+def about():
+    return "About page"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
