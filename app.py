633b2bce2e0163de91529bf476ce85f2ae1e6f55
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2021-12-19 00:22:59 +0400

    Building new project

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///client.db'
+db = SQLAlchemy(app)
+
+
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(300), nullable=False)
+    password = db.Column(db.String(10), nullable=False)
+
+    def __repr__(self):
+        return 'Account %r' % self.id
 
 
 @app.route('/')
 @app.route('/home')
 def index():
-    return "Gachimuchi"
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 
 @app.route('/user/<string:name>/<int:id>')
